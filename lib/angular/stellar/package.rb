f4168dbf76e81867eb5c75688823b013be314f5a
tomchentw <developer@tomchentw.com>
2013-12-24 00:38:04 +0800

    Use File.expand_path to read file

--- lib/angular/stellar/package.rb
+++ lib/angular/stellar/package.rb
@@ -2,7 +2,7 @@ require 'json'
 
 module Angular
   module Stellar
-    JSON.parse(File.read('package.json')).each do |key, value|
+    JSON.parse(File.read(File.expand_path('../../../package.json'))).each do |key, value|
       const_set(key.upcase, value)
     end
   end
